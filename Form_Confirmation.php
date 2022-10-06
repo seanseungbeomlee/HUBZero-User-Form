2fb7b953512d365eac3d90ba6f6918f74d4037ba
Sean Lee <109624710+seanseungbeomlee@users.noreply.github.com>
2022-10-06 13:32:16 -0500

    Uploading html and php files

---
+++ Form_Confirmation.php
@@ -0,0 +1,13 @@
+Welcome <?php echo $_POST["M1"]; ?><br>
+Your email address is: <?php echo $_POST["M2"]; ?>
+
+<!-- <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $M1 = $_POST["M1"];
+  if (empty($M1)) {
+    echo "Missing mass of star 1.";
+  } else {
+    echo "M1 = " $M1;
+  }
+}
+?> -->
