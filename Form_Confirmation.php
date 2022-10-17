69f5e9248c327b10c258776ef869f113d96c1e5c
seung-beom-lee <sl84@illinois.edu>
2022-10-17 10:41:04 -0500

    Render Latex.

--- Form_Confirmation.php
+++ Form_Confirmation.php
@@ -22,3 +22,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // ');
 }
 ?>
+
+<html>
+<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+<body>
+  $$x = {-b \pm \sqrt{b^2-4ac} \over 2a}.$$
+</body>
+</html>
