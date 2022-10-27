a1af1f6372c7eb0e86a6e2074c3bbe42e9df8fd4
seung-beom-lee <sl84@illinois.edu>
2022-10-27 11:54:00 -0500

    Embed latex in confimration page and added more parameters.

--- Form_Confirmation.php
+++ Form_Confirmation.php
@@ -3,16 +3,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $M1 = $_POST["M1"];
   $M2 = $_POST["M2"];
   $a = $_POST["a"];
+  $mta = $_POST["mta"];
+  $mtb = $_POST["mtb"];
+  $mtg = $_POST["mtg"];
   // $pdflatex = new PhpLatex_PdfLatex();
-  if (empty($M1) or empty($M2) or empty($a)) {
+  if (empty($M1) or empty($M2) or empty($a) or empty($mta) or empty($mtb) or empty($mtg)) {
     echo "Missing MESA paramters.";
-  } else {
-    echo "Mass of star 1: " . $M1;
-    echo "<br>";
-    echo "Mass of star 2: " . $M2;
-    echo "<br>";
-    echo "Orbital separation: " . $a;
-    echo "<br>";
   }
   // $pathToGeneratedPdf = $pdflatex->compileString('
   // \documentclass{article}
@@ -27,6 +23,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 <body>
-  $$x = {-b \pm \sqrt{b^2-4ac} \over 2a}.$$
+  Mass of star 1: \(<?php echo $M1?> M_\odot\) <br>
+  Mass of star 2: \(<?php echo $M2?> M_\odot\) <br>
+  Orbital separation: \(<?php echo $a?> R_\odot\) <br>
+  Mass Transfer Alpha: <?php echo $mta?> <br>
+  Mass Transfer Beta: <?php echo $mtb?> <br>
+  Mass Transfer Gamma: <?php echo $mtg?> <br>
 </body>
 </html>
